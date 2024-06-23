746da0770f5ce9b2fe14ec36ca0d78f99676664c
Mattias Joëlson <mattias@joelson.org>
2024-06-23 23:57:01 +0200

    Make scripts work.

--- scripts/feedsv4_startup.bat
+++ scripts/feedsv4_startup.bat
@@ -3,13 +3,11 @@
 SETLOCAL
 
 set scripts_dir="c:\Users\eljol\src\turfgame\scripts\"
-set jar_dir="c:\Users\eljol\.m2\repository\org\joelson\turf\resources\1.0.0"
-set jar_file="resources-1.0.0-jar-with-dependencies.jar"
+set jar_file="c:\Users\eljol\.m2\repository\org\joelson\turf\resources\1.0.0\resources-1.0.0-jar-with-dependencies.jar"
 set feed_drive=c:
 set feed_dir="c:\Users\eljol\src\turfgame_feedsv4"
 
 echo scripts:   %scripts_dir%
-echo jar dir:   %jar_dir%
 echo jar file:  %jar_file%
 echo drive:     %feed_drive%
 echo feed dir:  %feed_dir%
@@ -18,7 +16,7 @@ echo feed dir:  %feed_dir%
 :loop
 %feed_drive%
 cd %feed_dir%
-%scripts_dir%feedsv4.bat %jar_dir%\%jar_file%
+%scripts_dir%feedsv4.bat %jar_file%
 goto loop
 
 ENDLOCAL
